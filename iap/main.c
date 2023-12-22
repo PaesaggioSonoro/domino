38ed302c2b7c966a2f9ac8b3068663fc7f269810
nicola085 <71651518+nicola085@users.noreply.github.com>
2023-12-22 23:22:46 +0100

    cambiato generatessere

--- iap/main.c
+++ iap/main.c
@@ -122,16 +122,26 @@ while (1) {
 
 void generaSetTessere(Tessera *set, int *size) {
     int k = 0;
-    for (int i = 1; i <= 6; i++) {
-        for (int j = i; j <= 6; j++) {
-            set[k].lato1 = i;
-            set[k].lato2 = j;
-            set[k].utilizzata = 0;
-            k++;
-        }
+    // Definisci le tessere predefinite
+    int tessere_predefinite[][2] = {
+        {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6},
+        {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6},
+        {3, 3}, {3, 4}, {3, 5}, {3, 6},
+        {4, 4}, {4, 5}, {4, 6},
+        {5, 5}, {5, 6},
+        {6, 6}
+    };
+    int num_tessere_predefinite = sizeof(tessere_predefinite) / sizeof(tessere_predefinite[0]);
+
+    for (int i = 0; i < num_tessere_predefinite; i++) {
+        set[k].lato1 = tessere_predefinite[i][0];
+        set[k].lato2 = tessere_predefinite[i][1];
+        set[k].utilizzata = 0;
+        k++;
     }
     *size = k;
 }
+
 
 void assegnaTessereGiocatore(Tessera *giocatore, int nGiocatore, Tessera *set, int setSize) {
     srand(time(NULL)); // Inizializza il generatore di numeri casuali
@@ -201,7 +211,7 @@ int main() {
     int setSize;
     generaSetTessere(setTessere, &setSize);
 
-    int nGiocatore = generaNumeroCasuale(10); // Numero di tessere per il giocatore
+    int nGiocatore = generaNumeroCasuale(7); // Numero di tessere per il giocatore
    
     Tessera tessereGiocatore[nGiocatore];
     assegnaTessereGiocatore(tessereGiocatore, nGiocatore, setTessere, setSize);
